725cb8181d618c8645e084d80fabba6c142e30e7
Alexandra Saldana <alexsaldana9@gmail.com>
2017-10-21 15:50:07 -0400

    Added bootstrap and images have same size

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 u1 = User.create(name: 'Erik', allergies: 'Eggs', DOB: '2007, 11, 10', budget: 100.00)
 
-p1 = Product.create!(item: 'Sprout Organic Baby Food: Sweet Potato Apple Spinach', price: 1.59, allergens: 'Eggs', quantity: 100, image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwif6rWds4LXAhVIxCYKHZizDnUQjRwIBw&url=https%3A%2F%2Fwww.iherb.com%2Fpr%2FSprout-Organic-Baby-Food-Stage-2-Sweet-Potato-Apple-Spinach-5-Pouches-4-oz-113-g-Each%2F77782&psig=AOvVaw2Xzk_Zw_kBNT9ymSggnSs1&ust=1508698820930512')
+p1 = Product.create!(item: 'Sprout Organic Baby Food: Sweet Potato Apple Spinach', price: 1.59, allergens: 'Eggs', quantity: 100, image: 'https://www.images-iherb.com/l/SRT-01271-3.jpg')
 p2 = Product.create!(item: 'Sprite', price: 0.50, allergens: 'N/A', quantity: 50, image: 'https://ui3.assets-asda.com/g/v5/492/127/54492127_280_IDShot_3.jpeg')
 p3 = Product.create!(item: 'Plum Organics: Pear, Spinach & Pea', price: 1.99, allergens: 'Milk', quantity: 50, image: 'https://www.images-iherb.com/l/PLO-00119-7.jpg')
 
